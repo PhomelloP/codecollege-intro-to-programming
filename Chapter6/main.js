4cbbcb0bd03f4b58e6b33c55e97a39cd0e1ca745
PhomelloP <112708560+PhomelloP@users.noreply.github.com>
2025-01-20 16:15:24 +0200

    Add files via upload

---
+++ Chapter6/main.js
@@ -0,0 +1,21 @@
+var lemonChicken = false;
+            var beefWithBlackBean = false;
+            var sweetAndSourPork = false;
+            if (lemonChicken) {
+              console.log("Great! I'm having lemon chicken!");
+            } else if (beefWithBlackBean) {
+              console.log("I'm having the beef.");
+            } else if (sweetAndSourPork) {
+              console.log("OK, I'll have the pork.");
+            } else {
+              console.log("Well, I guess I'll have rice then.");
+            }
+            var sheepCounted = 0;
+            while (sheepCounted < 10) {
+              console.log("I have counted " + sheepCounted + " sheep!");
+              sheepCounted++;
+            }
+            var animals = ["Lion", "Flamingo", "Polar Bear", "Boa Constrictor"];
+for (var i = 0; i < animals.length; i++) {
+ console.log("This zoo contains a " + animals[i] + ".");
+}
